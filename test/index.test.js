280747c1be435ef1d88887fb4bb0ea612ea14444
rassi0429 <taitokokoa+rassi@gmail.com>
2022-02-16 14:04:15 +0900

    !! broke test

--- test/index.test.js
+++ test/index.test.js
@@ -27,7 +27,7 @@ test("J2E-copy", () => {
       ],
     })
   ).toEqual(
-    "l$#7$#v0$#123$#k0$#id$#t0$#number$#v1$#rhenium$#k1$#name$#t1$#string$#v2$#2$#k2$#sns.length$#t2$#number$#v3$#twitter$#k3$#sns_0_.type$#t3$#string$#v4$#@rhenium_vrc$#k4$#sns_0_.id$#t4$#string$#v5$#discord$#k5$#sns_1_.type$#t5$#string$#v6$#rhenium_75_#1015$#k6$#sns_1_.id$#t6$#string$#"
+    "al$#7$#v0$#123$#k0$#id$#t0$#number$#v1$#rhenium$#k1$#name$#t1$#string$#v2$#2$#k2$#sns.length$#t2$#number$#v3$#twitter$#k3$#sns_0_.type$#t3$#string$#v4$#@rhenium_vrc$#k4$#sns_0_.id$#t4$#string$#v5$#discord$#k5$#sns_1_.type$#t5$#string$#v6$#rhenium_75_#1015$#k6$#sns_1_.id$#t6$#string$#"
   );
 });
 
